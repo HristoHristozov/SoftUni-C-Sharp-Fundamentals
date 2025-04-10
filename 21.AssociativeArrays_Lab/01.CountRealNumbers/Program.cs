65e357aefdf4b1e6956a4bbfe0556ee25a82379a
Hristo Hristozov <hristozov95@gmail.com>
2025-04-10 17:08:28 +0300

    Uploading homework

--- 21.AssociativeArrays_Lab/01.CountRealNumbers/Program.cs
+++ 21.AssociativeArrays_Lab/01.CountRealNumbers/Program.cs
@@ -2,9 +2,28 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts.Add(number, 1);
+                }
+            }
+
+            foreach (var number in counts)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
         }
     }
 }
