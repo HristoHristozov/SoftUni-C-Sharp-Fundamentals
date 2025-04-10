78f0776d7eef6bc5da92850ce9b5abd346593fd4
Hristo Hristozov <hristozov95@gmail.com>
2025-04-10 17:07:16 +0300

    Uploading homework

---
+++ 21.AssociativeArrays_Lab/03.WordSynonyms/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _03.WordSynonyms
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int inputCount = int.Parse(Console.ReadLine());
+
+            var words = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < inputCount; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!words.ContainsKey(word))
+                {
+                    words.Add(word, new List<string>());
+                }
+                words[word].Add(synonym);
+
+            }
+
+            foreach (var pair in words)
+            {
+                Console.WriteLine($"{pair.Key} - {string.Join(", ", pair.Value)}");
+            }
+        }
+    }
+}
---
+++ 21.AssociativeArrays_Lab/04 WordFilter/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _04_WordFilter
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<string> list = Console.ReadLine().Split().Where(x => x.Length % 2 == 0).ToList();
+
+            foreach (string word in list)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
