b9bb91a9c9deaae6e3a8e546bb06d5f5fb1dd277
Hristo Hristozov <hristozov95@gmail.com>
2025-04-10 17:35:03 +0300

    Uploading homework

---
+++ 27.RegularExpressions_Lab/01.MatchFullName/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _01.MatchFullName
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+
+            string names = Console.ReadLine();
+
+            MatchCollection matchedNames = Regex.Matches(names, pattern);
+
+            foreach (Match m in matchedNames)
+            {
+                Console.Write(m.Value + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 27.RegularExpressions_Lab/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string pattern = @"(\+359([ -])2(\2)(\d{3})(\2)(\d{4}))\b";
+
+            string phoneNum = Console.ReadLine();
+
+            MatchCollection phoneMatches = Regex.Matches(phoneNum, pattern);
+
+            var matchedPhones = phoneMatches
+                .Cast<Match>()
+                .Select(x => x.Value.Trim())
+                .ToList();
+
+            Console.WriteLine(string.Join(", ", matchedPhones));
+        }
+    }
+}
